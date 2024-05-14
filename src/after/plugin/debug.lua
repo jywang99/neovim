390f079ccdf066f2f7c309b05fe74d26935f3ea1
Junyi Wang <junyi.wang.007@gmail.com>
2024-05-13 22:28:16 -0600

    debug continue keymap

--- src/after/plugin/debug.lua
+++ src/after/plugin/debug.lua
@@ -105,9 +105,9 @@ local mappings = {
 }
 whichkey.register(mappings, opts)
 
+vim.keymap.set("n", "<M-r>", ":DapContinue<CR>", { desc = "Debug continue" })
 vim.keymap.set("n", "<M-j>", ":DapStepOver<CR>", { desc = "Debug step over" })
 vim.keymap.set("n", "<M-l>", ":DapStepInto<CR>", { desc = "Debug step into" })
 vim.keymap.set("n", "<M-h>", ":DapStepOut<CR>", { desc = "Debug step out" })
-vim.keymap.set("n", "<M-n>", ":DapContinue<CR>", { desc = "Debug continue" })
 vim.keymap.set("n", "<M-k>", dapui.eval, { desc = "Float element" })
 
