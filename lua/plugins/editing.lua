9cdf009ebe5b9cf43b393ce1a66875b6993efdfa
junyi.wang <junyi.wang.007@gmail.com>
2025-01-22 02:03:55 -0700

    remove copilot chat

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -189,13 +189,5 @@ return {
             map('i', '<C-f>', acceptWord, {expr = true, remap = false})
         end
     },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "github/copilot.vim" },
-            { "nvim-lua/plenary.nvim", branch = "master" },
-        },
-        build = "make tiktoken",
-    },
 }
 
